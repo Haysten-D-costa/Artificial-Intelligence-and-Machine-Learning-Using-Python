ca1234e31fd2c941b97603b6e3b54e1949f44e0a
Haysten Dcosta <haystendc@gmail.com>
2023-02-25 16:09:17 +0530

    Added a new Test file

---
+++ test.py
@@ -0,0 +1,6 @@
+e_sequence = [5, 2, 5, 2, 5]
+f_sequence = [5, 2, 5, 2, 2]
+for items in e_sequence: # to print letter 'E', using stars '*'
+     print("*" * items)
+# for items in f_sequence: # to print letter 'F', using stars '*'
+#      print("*" * items)
